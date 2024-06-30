3a006e6d2cf8f34e2b67ce3e7d7e100eba315e2b
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-30 18:38:05 +0700

    [IMPROVE] Improved OrderForm for api autocomplete calls

--- vue-web-ex01/src/components/order/form/OrderForm.vue
+++ vue-web-ex01/src/components/order/form/OrderForm.vue
@@ -2,32 +2,30 @@
   <div class="container">
     <form @submit.prevent="addOrder">
         <div>
-            <label for="order-date">date</label>
+            <label for="order-date">Date</label>
             <input v-model="orderDate" type="date" id="order-date">
         </div>
         <div>
             <label for="customer-select">customer</label>
             <autocomplete
                 v-model="selectedCustomer"
-                :data="customerOptions"
+                :api-url="customerOptions"
                 field-name="name"
                 placeholder="Search for customer..."
-                @input="handleCustomerInput"
             />
         </div>
         <div>
             <label for="order-quantity-produc">quantity</label>
-            <input v-model="orderQuantity" type="number" id="order-quantity-product" min="1"/>
+            <input v-model.number="orderQuantity" type="number" id="order-quantity-product" min="1"/>
         </div>
         <div>
             <label for="order-product">product</label>
             <autocomplete
                 v-model="selectedProduct"
-                :data="productOptions"
+                :api-url="productOptions"
                 field-name="name"
                 placeholder="Search for product..."
-                @input="handleProductInput"
-            />
+                />
         </div>
         <div>
             <button class="btn btn-dark" type="submit">Order!</button>
@@ -37,39 +35,48 @@
 </template>
 
 <script>
-import Autocomplete from ''
-
+import { ref } from 'vue';
+import Autocomplete from '@/components/Autocomplete.vue';
 export default {
     components:{
-        Autocomplete
+        Autocomplete,
     },
-    data(){
-        return{
-            orderDate :'',
-            orderCustomer : '',
-            orderQuantity : 0,
-            orderProduct : '',
-            customers : [],
-            products : []
-        }
-    },
-    methods:{
-        addOrder(){
-            if(this.date && this.customer && this.quantity && this.product){
+    setup(){
+        const selectedCustomer = ref('');
+        const selectedProduct = ref('');
+        const customerApiUrl = 'API_URL/customers';
+        const productApiUrl = 'API_URL/products';
+        
+        const orderQuantity = ref(1);
+        const orderDate = ref(null);
+
+        const addOrder = () =>{
+            if(selectedCustomer && selectedProduct && orderQuantity.value > 0 && orderDate){
                 const newOrder = {
-                  date : this.orderDate,
-                  customer : this.orderCustomer,
-                  quantity : this.orderQuantity,
-                  product : this.orderProduct
+                  date : date.value ,
+                  customer : selectedCustomer.value,
+                  quantity : quantity.value,
+                  product : selectedProduct.value
                 }
                 this.$store.commit('ADD_TASK',newOrder)
-                this.orderDate = ''
-                this.orderCustomer = ''
-                this.orderQuantity = 0
-                this.orderProduct = ''
+                console.log(newOrder);
+                orderDate.value = ''
+                selectedCustomer.value = ''
+                orderQuantity.value = 1
+                selectedProduct.value = ''
             }
         }
-    }
+
+        return {
+            selectedCustomer,
+            selectedProduct,
+            customerApiUrl,
+            productApiUrl,
+            orderQuantity,
+            orderDate,
+            addOrder
+        }
+    },
 }
 </script>
 
