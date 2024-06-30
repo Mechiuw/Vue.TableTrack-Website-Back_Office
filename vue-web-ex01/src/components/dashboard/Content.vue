a5c4c114f843d47fbe2803c442a3945c1d96107c
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-30 17:29:41 +0700

    [FEATURE] Added OrderForm.vue

--- vue-web-ex01/src/components/dashboard/Content.vue
+++ vue-web-ex01/src/components/dashboard/Content.vue
@@ -1,8 +1,9 @@
 <script>
+
 </script>
 <template>
-    <div class="warning">
-        <h1 style="font-size: 500%;">UNDER MAINTAIN</h1>
+    <div>
+        
     </div>
 </template>
 <style>
---
+++ vue-web-ex01/src/components/order/form/OrderForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <form>
+        <div>
+            <label>date</label>
+            <input type="date"/>
+        </div>
+        <div>
+            <label>customer</label>
+            <select name="customer" id="customer-select">
+                <option value="">--select customer--</option>
+                <option :value="customer">{{ customer.name }}</option>
+            </select>
+        </div>
+        <div>
+            <label>quantity</label>
+            <input type="number"/>
+        </div>
+        <div>
+            <label>product</label>
+            <input type="text"/>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
