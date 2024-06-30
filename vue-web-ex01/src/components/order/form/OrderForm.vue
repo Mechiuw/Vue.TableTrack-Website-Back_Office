d76512cf4bc756426e226930a052087ee1f5b18e
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-30 17:45:03 +0700

    [FEATURE] Added Mutations to store

--- vue-web-ex01/src/components/order/form/OrderForm.vue
+++ vue-web-ex01/src/components/order/form/OrderForm.vue
@@ -3,22 +3,25 @@
     <form>
         <div>
             <label>date</label>
-            <input type="date"/>
+            <input v-model="orderDate" type="date" @keypress="addOrder"/>
         </div>
         <div>
             <label>customer</label>
-            <select name="customer" id="customer-select">
+            <select v-model="orderCustomer" name="customer" id="customer-select" @keypress="addOrder">
                 <option value="">--select customer--</option>
                 <option :value="customer">{{ customer.name }}</option>
             </select>
         </div>
         <div>
             <label>quantity</label>
-            <input type="number"/>
+            <input v-model="quantity" type="number" @keypress="addOrder"/>
         </div>
         <div>
             <label>product</label>
-            <input type="text"/>
+            <input v-model="product" type="text" @keypress="addOrder"/>
+        </div>
+        <div>
+            <button class="btn btn-dark" @click="addOrder"></button>
         </div>
     </form>
   </div>
@@ -26,7 +29,22 @@
 
 <script>
 export default {
+    data(){
+        orderDate :''
+        orderCustomer : ''
+        quantity : 0
+        product : ''
 
+    },
+    methods:{
+        addOrder(){
+            if(this.date && this.customer && this.quantity && this.product){
+                const newOrder = {
+                    
+                }
+            }
+        }
+    }
 }
 </script>
 
---
+++ vue-web-ex01/src/store/store.js
@@ -0,0 +1,37 @@
+export const state = () => {
+    orders = []
+}
+
+export const mutations = {
+    ADD_ORDER(state,order){
+        state.order = [order,...state.orders]
+    },
+    REMOVE_ORDER(state,order){
+        const index = state.orders.indexOf(order)
+        if(index !== -1){
+            state.orders.splice(index,1);
+        }
+    },
+    UPDATE_ORDER(state,order){
+        const index = state.orders.indexOf(order)
+        if(index !== -1){
+            state.orders[index] = order;
+        }
+    },
+    GET_ALL_ORDER(state,order){
+        if(isEmpty(state.orders) != true){
+            return [...state.orders]
+        }
+    },
+    GET_BY_ID(state,order){
+        const index = state.orders.indexOf(order)
+        if(index !== -1){
+            return [...state.orders]
+        }
+    }
+
+}
+
+function isEmpty(arx){
+    return arx === 0;
+}
