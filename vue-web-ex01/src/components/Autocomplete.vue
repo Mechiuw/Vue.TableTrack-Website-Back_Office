47694782f0184fa8834182d38b68aae52bd008f7
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-30 18:15:05 +0700

    [WIP] Work In Progress for OrderForm fetch API

---
+++ vue-web-ex01/src/components/Autocomplete.vue
@@ -0,0 +1,124 @@
+<!-- Autocomplete.vue -->
+<template>
+    <div class="autocomplete">
+      <input
+        type="text"
+        v-model="inputValue"
+        @input="handleInput"
+        @keydown.down="handleArrowDown"
+        @keydown.up="handleArrowUp"
+        @keydown.enter="selectItem"
+      />
+      <ul v-show="showDropdown" class="autocomplete-dropdown">
+        <li
+          v-for="(item, index) in filteredOptions"
+          :key="index"
+          @click="selectOption(item)"
+          :class="{ active: index === selectedOptionIndex }"
+        >
+          {{ item[fieldName] }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  import axios from 'axios';
+  
+  export default {
+    props: {
+      apiUrl: String,
+      fieldName: String,
+      value: null,
+    },
+    emits: ['update:value'],
+    setup(props, { emit }) {
+      const inputValue = ref('');
+      const showDropdown = ref(false);
+      const selectedOptionIndex = ref(-1);
+      const filteredOptions = ref([]);
+  
+      const handleInput = async () => {
+        try {
+          const response = await axios.get(`${props.apiUrl}?search=${inputValue.value}`);
+          filteredOptions.value = response.data;
+          showDropdown.value = true;
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      const handleArrowDown = () => {
+        if (selectedOptionIndex.value < filteredOptions.value.length - 1) {
+          selectedOptionIndex.value++;
+        }
+      };
+  
+      const handleArrowUp = () => {
+        if (selectedOptionIndex.value > 0) {
+          selectedOptionIndex.value--;
+        }
+      };
+  
+      const selectOption = (option) => {
+        inputValue.value = option[props.fieldName];
+        emit('update:value', option);
+        showDropdown.value = false;
+      };
+  
+      const selectItem = () => {
+        if (selectedOptionIndex.value !== -1) {
+          selectOption(filteredOptions.value[selectedOptionIndex.value]);
+        }
+      };
+  
+      return {
+        inputValue,
+        showDropdown,
+        selectedOptionIndex,
+        filteredOptions,
+        handleInput,
+        handleArrowDown,
+        handleArrowUp,
+        selectOption,
+        selectItem,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .autocomplete {
+    position: relative;
+    width: 100%;
+  }
+  
+  .autocomplete input {
+    width: 100%;
+    padding: 8px;
+    font-size: 16px;
+  }
+  
+  .autocomplete .autocomplete-dropdown {
+    position: absolute;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    z-index: 1;
+    width: 100%;
+    max-height: 150px;
+    overflow-y: auto;
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .autocomplete .autocomplete-dropdown li {
+    padding: 8px;
+    cursor: pointer;
+  }
+  
+  .autocomplete .autocomplete-dropdown li.active {
+    background-color: #ddd;
+  }
+  </style>
+  
--- vue-web-ex01/src/components/order/form/OrderForm.vue
+++ vue-web-ex01/src/components/order/form/OrderForm.vue
@@ -1,47 +1,72 @@
 <template>
   <div class="container">
-    <form>
+    <form @submit.prevent="addOrder">
         <div>
-            <label>date</label>
-            <input v-model="orderDate" type="date" @keypress="addOrder"/>
+            <label for="order-date">date</label>
+            <input v-model="orderDate" type="date" id="order-date">
         </div>
         <div>
-            <label>customer</label>
-            <select v-model="orderCustomer" name="customer" id="customer-select" @keypress="addOrder">
-                <option value="">--select customer--</option>
-                <option :value="customer">{{ customer.name }}</option>
-            </select>
+            <label for="customer-select">customer</label>
+            <autocomplete
+                v-model="selectedCustomer"
+                :data="customerOptions"
+                field-name="name"
+                placeholder="Search for customer..."
+                @input="handleCustomerInput"
+            />
         </div>
         <div>
-            <label>quantity</label>
-            <input v-model="quantity" type="number" @keypress="addOrder"/>
+            <label for="order-quantity-produc">quantity</label>
+            <input v-model="orderQuantity" type="number" id="order-quantity-product" min="1"/>
         </div>
         <div>
-            <label>product</label>
-            <input v-model="product" type="text" @keypress="addOrder"/>
+            <label for="order-product">product</label>
+            <autocomplete
+                v-model="selectedProduct"
+                :data="productOptions"
+                field-name="name"
+                placeholder="Search for product..."
+                @input="handleProductInput"
+            />
         </div>
         <div>
-            <button class="btn btn-dark" @click="addOrder"></button>
+            <button class="btn btn-dark" type="submit">Order!</button>
         </div>
     </form>
   </div>
 </template>
 
 <script>
-export default {
-    data(){
-        orderDate :''
-        orderCustomer : ''
-        quantity : 0
-        product : ''
+import Autocomplete from ''
 
+export default {
+    components:{
+        Autocomplete
+    },
+    data(){
+        return{
+            orderDate :'',
+            orderCustomer : '',
+            orderQuantity : 0,
+            orderProduct : '',
+            customers : [],
+            products : []
+        }
     },
     methods:{
         addOrder(){
             if(this.date && this.customer && this.quantity && this.product){
                 const newOrder = {
-                    
+                  date : this.orderDate,
+                  customer : this.orderCustomer,
+                  quantity : this.orderQuantity,
+                  product : this.orderProduct
                 }
+                this.$store.commit('ADD_TASK',newOrder)
+                this.orderDate = ''
+                this.orderCustomer = ''
+                this.orderQuantity = 0
+                this.orderProduct = ''
             }
         }
     }
@@ -49,5 +74,5 @@ export default {
 </script>
 
 <style>
-
+@import url('./OrderFormStyle.css');
 </style>
